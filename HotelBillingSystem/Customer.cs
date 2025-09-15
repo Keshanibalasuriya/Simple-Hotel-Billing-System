8c350003313b4e384c142417f3ec2b80957cf3b1
Keshani Balasuriya <151428720+Keshanibalasuriya@users.noreply.github.com>
2025-09-15 09:27:12 +0530

    Hotel Billing

---
+++ HotelBillingSystem/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBillingSystem
+{
+    internal class Customer
+    {
+        public string No { get; set; }
+        public string Name { get; set; }
+
+        public DateTime checkInDate { get; set; }
+
+        public string RoomType { get; set; }
+
+        public double BarLoungeCharge { get; set; }
+
+        public double RestaurantCharges { get; set; }
+
+        public double WellnessCharges {get; set;}
+
+        public double AirportPickupCharges { get; set; }
+
+        public double NetPaymentCharges { get; set; }
+
+    }
+
+}
---
+++ HotelBillingSystem/CustomerList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBillingSystem
+{
+    internal class CustomerList
+    {
+        public static List<Customer> customers = new List<Customer>();
+
+
+        public static void  AddCustomer(Customer customer) { 
+            customers.Add(customer);
+        }
+
+        public static List<Customer> GetCustomers() { 
+            return customers;
+        }
+
+        public static int GetNextCustomerNo()
+        {
+            return customers.Count + 1;
+        }
+
+
+    }
+}
